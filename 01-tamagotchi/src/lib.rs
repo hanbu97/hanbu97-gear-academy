4fb549886da9a9dffc5a4fadae132ee0cf47d9b7
hanbu97 <hanbu97@proton.me>
2023-09-11 23:02:28 +0800

    01-tamagotchi

--- 01-tamagotchi/src/lib.rs
+++ 01-tamagotchi/src/lib.rs
@@ -1,18 +1,43 @@
 #![no_std]
 
-use gstd::prelude::*;
+use gstd::{debug, exec, msg, prelude::*};
+use tmg1_io::{Tamagotchi, TmgAction, TmgEvent};
+
+static mut ENTITY: Option<Tamagotchi> = None;
 
 #[no_mangle]
 extern "C" fn init() {
-    // TODO: 5️⃣ Initialize the Tamagotchi program
+    let name: String = msg::load().expect("Error in loading name");
+    let birth = exec::block_timestamp();
+
+    unsafe {
+        ENTITY = Some(Tamagotchi::new(name, birth));
+    }
 }
 
 #[no_mangle]
 extern "C" fn handle() {
-    // TODO: 6️⃣ Add handling of `Name` and `Age` actions
+    let input_message: TmgAction = msg::load().expect("Error in loading TmgAction");
+    let entity: &Tamagotchi = unsafe { ENTITY.as_ref().expect("The contract is not initialized") };
+
+    match input_message {
+        TmgAction::Name => {
+            let name = entity.name.clone();
+            debug!("Name: {}", &name);
+            msg::reply(TmgEvent::Name(name), 0).expect("Error in sending reply");
+        }
+        TmgAction::Age => {
+            let now = exec::block_timestamp();
+            let birth = entity.date_of_birth;
+            let age = now - birth;
+            debug!("{}-{} Age: {}", now, birth, age);
+            msg::reply(TmgEvent::Age(age), 0).expect("Error in sending reply");
+        }
+    }
 }
 
 #[no_mangle]
 extern "C" fn state() {
-    // TODO: 7️⃣ Return the Tamagotchi state
+    let entity: &Tamagotchi = unsafe { ENTITY.as_ref().expect("The contract is not initialized") };
+    msg::reply(entity, 0).expect("Error in sending reply");
 }
--- 01-tamagotchi/tests/smoke.rs
+++ 01-tamagotchi/tests/smoke.rs
@@ -1,10 +1,43 @@
-use gtest::{Program, System};
+use gtest::{Log, Program, System};
+use tmg1_io::{TmgAction, TmgEvent};
 
 #[test]
 fn smoke_test() {
     let sys = System::new();
     sys.init_logger();
-    let _program = Program::current(&sys);
+    let program = Program::current(&sys);
 
-    // TODO: 8️⃣ Test the program initialization and message handling
+    // init
+    let birth = sys.block_timestamp();
+    let res = program.send(2, String::from("babygear"));
+    assert!(!res.main_failed());
+
+    // get name
+    let res = program.send(4, TmgAction::Name);
+    let expected_res: Log = Log::success_builder(gtest::SuccessReplyReason::Manual)
+        .source(1)
+        .dest(4)
+        .payload(TmgEvent::Name(String::from("babygear")));
+    assert!(res.contains(&expected_res));
+
+    // get age
+    let res = program.send(2, TmgAction::Age);
+    let expected_res = Log::success_builder(gtest::SuccessReplyReason::Manual)
+        .source(1)
+        .dest(2)
+        .payload(TmgEvent::Age(0));
+    assert!(res.contains(&expected_res));
+
+    // grow
+    sys.spend_blocks(100);
+    let now = sys.block_timestamp();
+
+    // get age
+    let res = program.send(2, TmgAction::Age);
+    let expected_res = Log::success_builder(gtest::SuccessReplyReason::Manual)
+        .source(1)
+        .dest(2)
+        .payload(TmgEvent::Age(now - birth));
+
+    assert!(res.contains(&expected_res));
 }
